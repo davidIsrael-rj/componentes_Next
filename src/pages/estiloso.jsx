dac37f0848b9807152403f16ddf6166537d1e15a
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-24 06:36:52 -0300

    CSS Modularizado

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/Estiloso.module.css"
+
+export default function Estiloso(){
+    return(
+        <div className={styles.roxo}>
+            <h1>Estiloso</h1>
+            <h2>Estilo usando CSS Módulos</h2>
+        </div>
+    )
+}
