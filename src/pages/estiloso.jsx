7a3981d88e9b8395ced546cfccbc388da36526cc
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-25 06:14:50 -0300

    Navegação entre Componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,8 +1,10 @@
 import styles from "@/styles/Estiloso.module.css"
+import Link from "next/link"
 
 export default function Estiloso(){
     return(
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estiloso</h1>
             <h2>Estilo usando CSS Módulos</h2>
         </div>
