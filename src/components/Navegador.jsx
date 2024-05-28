4a22d43f3d3a1d1ced88698ee402e802e4a86823
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-28 15:54:38 -0300

    Navegação Simples

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -3,12 +3,7 @@ import styles from "@/styles/Navegador.module.css"
 
 export default function Navegador(props) {
     return (
-        <div className={styles.navegador}
-            // style={{
-            //     backgroundColor:props.cor ?? 'dodgerblue'
-            
-            // }}
-        >
+        <div className={styles.navegador}>
 
             <Link href={props.link}>
                 {props.nome}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,17 @@ export default function Inicio(props) {
     return (
         <div style={{
             display: 'flex',
-            justifyContent: 'center',
+            flexDirection:"column",
+            justifyContent: 'flex-start',
             alignItems:'center',
             flexWrap: 'wrap',
+            padding:"20px",
             height: '100vh'
         }}>
             <Navegador link="/estiloso" nome="Estiloso"/>
             <Navegador link="/exemplo" nome="Exemplo"/>
             <Navegador link="/jsx" nome="JSX"/>
+            <Navegador link="/navegacao" nome="Navegação"/>
         </div>
     )
 }
