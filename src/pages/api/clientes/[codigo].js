fa561498540b498799b23bffee7469a55909ed7d
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-30 06:23:54 -0300

    Corrigindo erro da pagina JSX, <h1> não pode aparecer como filho de <h1>

--- src/pages/api/clientes/[codigo].js
+++ src/pages/api/clientes/[codigo].js
@@ -1,3 +1,15 @@
+// MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 
+// 11 close listeners added to [Server]. Use emitter.setMaxListeners() to increase limit
+// (Use `node --trace-warnings ...` to show where the warning was created)
+// caso ocorra esse erro acima utilize esse codico logo abaixo
+
+// import {EventEmitter} from 'events';
+// const emitter = new EventEmitter();
+// emitter.setMaxListeners(20);
+
+// por padrao são 10 ovites configurado, se precisar de mais é só configurar com o 
+// código acima
+
 export default function handler(req, res){
     const codigo = req.query.codigo
     res.status(200).json({
--- src/pages/jsx.jsx
+++ src/pages/jsx.jsx
@@ -2,14 +2,14 @@ import Layout from "@/components/Layout"
 
 export default function Jsx() {
     const titulo = <h1>JSX é um Conceito Central</h1>
-
+    const titulo2 = "JSX é um Conceito Central"
     function subtitulo() {
         return <h2>{"transformando em maiúscula".toUpperCase()}</h2>
     }
     return (
-        <Layout titulo={titulo}>
+        <Layout titulo={titulo2}>
             <div>
-                {titulo}
+            {titulo}
                 {subtitulo()}
                 <p>
                     {JSON.stringify({ nome: 'João', idade: 30 })}
